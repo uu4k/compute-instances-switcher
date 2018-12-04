89766028c813768bdb670b0f7f66b6391614ecdd
uu4k <t.shimizu1115@gmail.com>
2018-12-04 09:20:22 +0900

    create switcher.

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import json
 import base64
 import logging
 import sys
+import os
 import googleapiclient.discovery
 
 JSON_SCHEMA = json.load(
-     open('./schema.json'))
+    open('./schema.json'))
+
+PROJECT = os.environ.get('GCP_PROJECT')
+
+
 def switch_compute_instances(data, context):
 
     if 'data' not in data:
-        exit
+        sys.exit(1)
 
     payload = json.load(base64.b64decode(data['data']).decode('utf-8'))
 
@@ -21,20 +26,63 @@ def switch_compute_instances(data, context):
         logging.error(m)
         sys.exit(1)
 
-
     switch = payload['switch']
     target = payload['target']
 
     compute = googleapiclient.discovery.build('compute', 'v1')
+    switcher = new Switcher(compute, PROJECT)
 
-    instances = list_instances(compute)
+    if switch == switcher.ON:
+        switcher.on(target)
+    elif switch == switcher.OFF:
+        switcher.off(target)
 
-    # 同プロジェクトのインスタンスリスト取得
 
-    # targetの正規表現とマッチするかチェック
+class Switcher:
 
-    # マッチした場合はon/offに応じて起動・停止
+    ON = 'on'
+    OFF = 'off'
 
-def list_instances(compute):
-    result = compute.instances().list().execute()
-    return result['items'] if 'items' in result else None
+    def __init__(self, compute, project):
+        self.compute = compute
+        self.project = project
+
+    def on(self, target):
+        # filter構築
+        filter = self.__create_filter(self.ON, target)
+
+        # instances取得
+        instances = self.__list_instances(filter)
+
+        # 開始
+
+    def off(self, target):
+        # filter構築
+        filter = self.__create_filter(self.OFF, target)
+
+        # instances取得
+        instances = self.__list_instances(filter)
+
+        # 開始
+
+    def __list_instances(self, filter):
+        zones = self.__list_zones()
+        for zone in zones:
+            result = self.compute.instances().list(
+                project=self.project, zone=zone['name'], filter=filter).execute()
+
+        return result['items'] if 'items' in result else []
+
+    def __list_zones(self):
+        result = self.compute.zones().list(project=self.project).execute()
+        return result['items'] if 'items' in result else []
+
+    def __create_filter(self, switch, target):
+
+        filter = 'name:' + target
+        if switch == self.ON:
+            filter += ' status!=running'
+        elif switch == self.OFF:
+            filter += ' status=running'
+
+        return filter
